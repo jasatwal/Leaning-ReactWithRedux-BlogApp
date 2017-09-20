42fa60f4fcf69e7c62967b7c42e19c6dd13b3a6d
Jaspreet Atwal <jatwal@capaxglobal.com>
2017-09-20 21:23:56 +0100

    Simple form.

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,11 +1,39 @@
 import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
 
 class PostsNew extends Component {
     render() {
         return (
-            <div>PostsNew</div>
+            <form>
+                <Field 
+                    label="Title for Post"
+                    name="title" 
+                    component={this.renderField} />
+                <Field 
+                    label="Categories"
+                    name="categories" 
+                    component={this.renderField} />
+                <Field 
+                    label="Post Content"
+                    name="content" 
+                    component={this.renderField} />                    
+            </form>
         )
+    }
+
+    renderField(field) {
+        return (
+            <div className="form-group">
+                <label>{field.label}</label>
+                <input 
+                    className="form-control"
+                    type="text"
+                    {...field.input} />
+            </div>
+        );
     }
 }
 
-export default PostsNew;
+export default reduxForm({
+    form: "PostsNewForm" // form name
+})(PostsNew);
